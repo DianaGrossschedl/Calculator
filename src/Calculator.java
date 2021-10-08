3e71b6d2be3fc9ac5f275cbd07ff41518d48a3fc
Diana <51800713@edu.campus02.at>
2021-10-08 19:58:53 +0200

    Variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,20 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+        int a = 1;
+        System.out.println(a);
+
+        double d = 2;
+        System.out.println(d);
+
+        boolean b = true;
+        System.out.println(b);
+
+        char c = 'd';
+        System.out.println(c);
+
+        String s = "Text";
+        System.out.println(s);
+
     }
 }
